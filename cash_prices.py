595122045961b989e65cc21c5e15aab9195bbaae
Tyson Brost <tyson.brost@gmail.com>
2023-09-27 17:21:01 -0600

    Update cash_prices.py

--- cash_prices.py
+++ cash_prices.py
@@ -35,7 +35,7 @@ def check_password():
 if check_password():
 
     st.title("Cash Prices")
-    st.write("Below is the cash price for wheat in different strands in different parts of the state. Select the region and strand you are interested in and the table and the chart will adjust to your input. This data is separated into rows by week of the year. In many areas and strands there is no information recorded, but for every week there should be an aggregate value for referance")
+    #st.write("Below is the cash price for wheat in different strands in different parts of the state. Select the region and strand you are interested in and the table and the chart will adjust to your input. This data is separated into rows by week of the year. In many areas and strands there is no information recorded, but for every week there should be an aggregate value for referance")
 
 
     # GitHub repository information
@@ -77,6 +77,17 @@ if check_password():
     df_pivot = df_pivot.reset_index(names=['Attribute', 'week of year', 'year'])
     st.dataframe(df_pivot)
 
+    filename= f'{ATTRIBUTE}_{CITY}_data.csv'
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+    csv = convert_df(df_pivot)
+    st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name=filename,
+    mime='text/csv',
+)
     # Allow users to select summary columns for the line chart
     selected_columns = st.multiselect(
         'Select summary columns for the line chart',
@@ -92,12 +103,12 @@ if check_password():
 
 
 
-    st.subheader('Charted over the Year')
-    st.write("Seelct which metrics you want to see, you can select multiple")
+    #st.subheader('Charted over the Year')
+    #st.write("Seelct which metrics you want to see, you can select multiple")
 
 
-    def link_to_github():
-        href = '<a href="https://github.com/Lusk27/app_display/tree/main/Data" target="_blank">Link to GitHub</a>'
-        return href
+    #def link_to_github():
+    #    href = '<a href="https://github.com/Lusk27/app_display/tree/main/Data" target="_blank">Link to GitHub</a>'
+    #    return href
 
-    st.markdown(link_to_github(), unsafe_allow_html=True)
+    #st.markdown(link_to_github(), unsafe_allow_html=True)
