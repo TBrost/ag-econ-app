57d298a35528b1cf1d24780100dfe4538cedaff3
Tyson Brost <tyson.brost@gmail.com>
2023-09-27 16:41:14 -0600

    update

--- cash_prices.py
+++ cash_prices.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly_express as px
 import requests
 import io
 st.set_page_config(page_title="Wheat Charts", page_icon="🌾")
